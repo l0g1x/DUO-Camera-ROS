9ed5a5b7425da7198005319243a0e2079a8fe431
L0g1x-Krystian <krgebis@gmail.com>
2015-03-22 20:58:22 -0500

    Correcting DUO image header time stamp. Now using the pFrameData pointer to specify the time stamp.

--- src/driverDUOstereo.cpp
+++ src/driverDUOstereo.cpp
@@ -45,11 +45,10 @@ DUOStereoDriver::~DUOStereoDriver(void)
 
 void DUOStereoDriver::fillDUOImages(sensor_msgs::Image& leftImage, sensor_msgs::Image& rightImage, const PDUOFrame pFrameData)
 {
-	ros::Time timeNow 			= ros::Time::now();
 
-	leftImage.header.stamp 		= timeNow;
+	leftImage.header.stamp 		= ros::Time( double(pFrameData->timeStamp) * 1.e-4);
 	leftImage.header.frame_id 	= _camera_frame;
-	rightImage.header.stamp 	= timeNow;
+	rightImage.header.stamp 	= leftImage.header.stamp;
 	rightImage.header.frame_id 	= _camera_frame;
 
 	// Fill the left image message, the step size needs to be the amount of pixels
